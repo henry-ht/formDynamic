0b5d0ae543bd06d9f39f637fe413136f5f028e79
henrytorres3c <henry.torres@3creatives.com.co>
2022-10-09 12:54:33 -0500

    dynamic validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {FormControl,
+  FormGroup,
+  FormBuilder,
+  FormArray,
+  Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formDynamic';
+  exampleForm: FormGroup;
+
+  constructor(private fb: FormBuilder){
+    this.exampleForm = this.fb.group(
+      {
+        email: [null, [Validators.required]],
+        phone: [null],
+        tickets: this.fb.array([this.createTicket()])
+      }
+    )
+  }
+
+  createTicket():FormGroup{
+    return this.fb.group({
+      name:[null, [Validators.required, Validators.minLength(3)]],
+      age: [null,[Validators.required]]
+    })
+  }
+
+  get tickets():FormArray{
+    return <FormArray> this.exampleForm.get("tickets");
+  }
+
+  addTicket() {
+    this.tickets.push(this.createTicket());
+  }
+
+  deleteTicket(i: number) {
+    this.tickets.controls.splice(i,1);
+    this.tickets.updateValueAndValidity();
+  }
+
+  ngOnInit(){
+
+  }
+
+  save(){
+    console.log(this.tickets.value)
+  }
 }
